acce7490b89e88a59a34744e4cc0740b6415969b
Johnny Arguedas <johnnyarguedas@hotmail.com>
2017-12-13 03:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,7 @@ function happyHolidayTo(holiday, name){
 function holidayCountdown(holiday, days){
    return `It's ${days} days until ${holiday}!`
 }
+
+function holidayCountdown(holiday, days){
+  return
+}
