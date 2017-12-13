e8ba5aed293e84be2b438ad1484709504d9e8ecf
Johnny Arguedas <johnnyarguedas@hotmail.com>
2017-12-13 03:06:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function happyHolidays(string){
  return "Happy holidays!"
 }
 
-function happyHolidaysTo (name) {
- return happyHolidaysTo('you!')
+function happyHolidaysTo (string) {
+ return string.happyHolidaysTo('you!')
 }
